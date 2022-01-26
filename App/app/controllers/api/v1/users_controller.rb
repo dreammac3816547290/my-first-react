c529ec4ae55156ea5bfb1ed11dee74bf34ea63b4
dreammac3816547290 <95905466+dreammac3816547290@users.noreply.github.com>
2022-01-26 07:37:27 +0700

    Update users_controller.rb
    Fixed a little mistake in the code

--- App/app/controllers/api/v1/users_controller.rb
+++ App/app/controllers/api/v1/users_controller.rb
@@ -61,6 +61,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.permit(:name)
+      params.permit(:name :password :tags :tasks :pending :blocks)
     end
 end
